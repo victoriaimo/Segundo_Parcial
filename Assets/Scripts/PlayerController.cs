e0e0b67d60889c1bf6c627faa229fe9674fc970d
victoriaimo <svictoriaimo@gmail.com>
2026-07-02 09:59:56 -0300

    Mejora controlador del jugador
    - Agrega movimiento relativo a la cámara.
    - Agrega salto.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public float velocidad = 5f;
     public float velocidadCorrer = 8f;
+    public float fuerzaSalto = 5f;
+
+    public Transform camaraJugador;
 
     private Rigidbody rb;
 
     private float movimientoX;
     private float movimientoZ;
+
+    private bool estaEnSuelo = true;
 
     void Start()
     {
@@ -21,6 +26,12 @@ public class PlayerController : MonoBehaviour
     {
         movimientoX = Input.GetAxis("Horizontal");
         movimientoZ = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space) && estaEnSuelo)
+        {
+            rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+            estaEnSuelo = false;
+        }
     }
 
     void FixedUpdate()
@@ -32,8 +43,25 @@ public class PlayerController : MonoBehaviour
             velocidadActual = velocidadCorrer;
         }
 
-        Vector3 movimiento = new Vector3(movimientoX, 0, movimientoZ);
+        Vector3 adelante = camaraJugador.forward;
+        Vector3 derecha = camaraJugador.right;
+
+        adelante.y = 0;
+        derecha.y = 0;
+
+        adelante.Normalize();
+        derecha.Normalize();
+
+        Vector3 movimiento = adelante * movimientoZ + derecha * movimientoX;
 
         rb.MovePosition(transform.position + movimiento * velocidadActual * Time.fixedDeltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            estaEnSuelo = true;
+        }
     }
 }
